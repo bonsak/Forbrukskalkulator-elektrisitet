0f0e19c3a01e037e609bd0e05f92810fb09a5d4a
Bonsak Schiledrop <1322824+bonsak@users.noreply.github.com>
2024-10-21 15:46:58 +0200

    Typo

--- src/components/ForbuksGrid.jsx
+++ src/components/ForbuksGrid.jsx
@@ -1,13 +1,11 @@
 import './ForbruksGrid.css'
 
 function ForbruksGrid() {
-
   const kolonner = []
-  for(var i = 0; i<=11 ;i ++){
+  for (var i = 0; i <= 11; i++) {
     kolonner.push(i * 2)
   }
-  const rader = [0,1]
-
+  const rader = [0, 1]
 
   // kolonner.map((item, index) =>{
   //   kolonner[index] = index * 2
@@ -18,14 +16,21 @@ function ForbruksGrid() {
     <div className={'forbruksgrid-wrapper'}>
       {kolonner.map((item, index) => {
         // console.log(item)
-        return <div key={index} className={'forbruks-kolonne'}>
-         {rader.map((rad, index) => (
-            <div key={rad} className={'forbruks-slot'}></div>
-      ))}
-        </div>
-    })}
+        return (
+          <div
+            key={index}
+            className={'forbruks-kolonne'}
+          >
+            {rader.map((rad) => (
+              <div
+                key={rad}
+                className={'forbruks-slot'}
+              ></div>
+            ))}
+          </div>
+        )
+      })}
     </div>
   )
-  
 }
 export default ForbruksGrid
