4a16240ef032e99ac7ebb27dbfb0a19455c4faa9
Bonsak Schiledrop <1322824+bonsak@users.noreply.github.com>
2024-10-25 00:38:32 +0200

    GSAP draggable litt framskritt

--- src/components/AktivitetsGrid.jsx
+++ src/components/AktivitetsGrid.jsx
@@ -10,94 +10,41 @@ import { create } from 'zustand'
 
 gsap.registerPlugin(Draggable)
 
-function AktivitetsGrid(bounds, ...delegated) {
+function AktivitetsGrid() {
   const aktiviteter = useRef()
   const aktivitet = useRef([])
   const forbruksSlotRef = useRef([])
 
   const [targetSlot, setTargetSlot] = useState()
-  // const { targetSlot, setTargetSlot, resetTargetSlot } = useTargetSlotStore()
 
   // const { contextSafe } = useGSAP({ scope: aktiviteter })
 
   useGSAP(() => {
-    const drag = Draggable.create(aktivitet.current, {
+    Draggable.create(aktivitet.current, {
       bounds: window,
-      onPress: (e) => {
-        // setTargetSlot(e.target)
-        // console.log('on Press: ', e.target)
-        //   // gsap.to(e.target, {
-        //   //   scale: 1.1,
-        //   // })
-        //   // gsap.to(e.target, {
-        //   //   duration: 0.1,
-        //   //   rotate: 0,
-        //   // })
-      },
+      // onPress: (e) => {
+      // },
       onDrag: (e) => {
-        // console.log(
-        //   'refs: ',
-        //   e.target,
-        //   aktivitet.current,
-        //   forbruksSlotRef.current
-        // )
-        // console.log('Slots lengde', forbruksSlotRef.current.length)
-
-        var i = forbruksSlotRef.current.length
-        while (--i >= 0) {
-          if (Draggable.hitTest(e.target, forbruksSlotRef.current[i], 12)) {
-            // gsap.to(e.target, {
-            //   rotate: -10,
-            // })
-            // let aktivitetsBB = e.target.getBoundingClientRect()
-            // let forbruksBB = forbruksSlotRef.current.getBoundingClientRect()
-            setTargetSlot(forbruksSlotRef.current[i])
-            // x = '+=' + (forbruksBB.left - aktivitetsBB.left)
-            // y = '+=' + (forbruksBB.top - aktivitetsBB.top)
-            // useTargetSlotStore((state) => state.bears)
-            // const ttt = setTargetSlot(forbruksSlotRef.current[i])
-            console.log('Loop Hit target object', targetSlot)
-            // setTargetSlot(forbruksSlotRef.current[i])
-
-            // }))
-            // return
-          } else {
-            // resetTargetSlot()
-            // setTargetSlot(null)
-            // gsap.to(e.target, {
-            //   duration: 0.1,
-            //   x: 0,
-            //   y: 0,
-            // })
-            console.log('No hit', targetSlot)
+        for (let y = 0; y < forbruksSlotRef.current.length; y++) {
+          if (Draggable.hitTest(e.target, forbruksSlotRef.current[y])) {
+            // let targetPos = gsap.getProperty(
+            //   forbruksSlotRef.current[y],
+            //   'x',
+            //   'px'
+            // )
+            // setTargetSlot(forbruksSlotRef.current[y])
+            console.log(
+              'targetSlot rect',
+              forbruksSlotRef.current[y].getBoundingClientRect()
+            )
+            break
           }
         }
       },
-      onDragEnd: (e) => {
-        // let x = 0
-        // let y = 0
-        // let aktivitetsBB = e.target.getBoundingClientRect()
-        // let forbruksBB = forbruksSlotRef.current.getBoundingClientRect()
-        // x = '+=' + (forbruksBB.left - aktivitetsBB.left)
-        // y = '+=' + (forbruksBB.top - aktivitetsBB.top)
-        // console.log('Forbruks BB: ', forbruksBB)
-        // console.log('Aktivitets BB: ', aktivitetsBB)
-        // gsap.to(e.target, {
-        //   duration: 0.1,
-        //   rotation: 0,
-        //   x: x,
-        //   y: y,
-        // })
-        // console.log('Targetslot: ', targetSlot)
-      },
+      onDragEnd: (e) => {},
     })
-    // const increasePopulation = useStore((state) => state.increasePopulation)
-    // const updateTarget = useTargetSlotStore((forbruksSlotRef.current[i]) => set({targetSlot: forbruksSlotRef.current[i]}));
   }, [])
-  // function updateTargetSlot(target) {
-  //   setTargetSlot(target)
-  //   console.log('Her', targetSlot)
-  // }
+
   function handleEnter(e) {
     gsap.to(e.target, {
       duration: 0.1,
